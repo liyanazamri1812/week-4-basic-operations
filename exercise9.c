2f19c9208a3ef69b2265f348ecd78442dadb708d
aqillakhamis <aqillakhamis@gmail.com>
2022-04-11 09:40:48 +0800

    update tutorial week 4

---
+++ exercise9.c
@@ -0,0 +1,19 @@
+//documentation section 
+/* Exercise 9 - Update Operations */
+
+//pre-processor section 
+#include<stdio.h>
+
+//global variable section 
+
+
+//main function section 
+int main(){
+    //write here your program
+
+
+    return 0;
+}
+
+
+//user-defined section 
